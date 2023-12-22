43c37cb76f73c7094d67606cde53fb91cd9aa629
XiaoyangJin <jin.xiaoya@northeastern.edu>
2023-12-22 11:12:39 -0500

    set up

---
+++ connectPostgresql/src/test/java/com/xy/connectPostgresql/ConnectPostgresqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xy.connectPostgresql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectPostgresqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
